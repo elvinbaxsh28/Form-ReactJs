6dea7bad5b8e1834d001644da9ea1283f7cd68d7
User <elvinbaxsheliyev000@gmail.com>
2023-03-18 21:30:34 +0400

    lastCommit

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,13 @@
-import React, {useState} from "react";
+import React, {useState , useRef, useEffect} from "react";
 import Modal from "react-bootstrap/Modal";
 import ShoppingCart from './ShoppingCart'
+
 
 const MyComponent = () => {
     const [showModal,
         setShowModal] = useState(false);
 
+    
     const handleShowModal = () => {
         setShowModal(true);
     };
@@ -16,20 +18,31 @@ const MyComponent = () => {
 
     return (
         <div>
-            <button onClick={handleShowModal}>Add User</button>
+            <button onClick={handleShowModal} className="button-87 d-grid gap-2">Add User</button>
 
-            <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal  show={showModal} onHide={handleCloseModal} className="my-modal">
                 <Modal.Header closeButton>
-                    <Modal.Title>Add User</Modal.Title>
+                    <Modal.Title className="my-modal-title">Add User</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body className="my-modal-body">
                     <ShoppingCart/>
                 </Modal.Body>
                 <Modal.Footer>
-                    <button onClick={handleCloseModal}>Close</button>
-                    <button onClick={handleCloseModal}>Save</button>
+                    <button onClick={handleCloseModal} className="my-modal-button">Close</button>
+                    <button onClick={handleCloseModal} className="my-modal-button">Save</button>
                 </Modal.Footer>
             </Modal>
+
+            {showModal && (
+        <style>
+          {`.modal-dialog {
+            max-width: 100vw !important;
+            max-height: 100vh !important;
+          }`}
+        </style>
+      )}
+
+        
         </div>
     );
 };
